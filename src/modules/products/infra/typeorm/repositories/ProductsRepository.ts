4b7b02718932551103344dadbf3653f1215bc7c1
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-12-12 12:03:52 -0300

    Products settings and features

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -2,12 +2,9 @@ import { getRepository, Repository, In } from 'typeorm';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
+import IFindProductsDTO from '@modules/products/dtos/IFindProductsDTO';
 import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
 import Product from '../entities/Product';
-
-interface IFindProducts {
-  id: string;
-}
 
 class ProductsRepository implements IProductsRepository {
   private ormRepository: Repository<Product>;
@@ -21,15 +18,23 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const findProduct = await this.ormRepository.findOne({ where: { name } });
+
+    return findProduct;
   }
 
-  public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+  public async findAllById(products: IFindProductsDTO[]): Promise<Product[]> {
+    const findProducts = await this.ormRepository.findByIds(products);
+
+    return findProducts;
   }
 
   public async updateQuantity(
--- src/modules/products/repositories/IProductsRepository.ts
+++ src/modules/products/repositories/IProductsRepository.ts
@@ -1,15 +1,12 @@
 import Product from '../infra/typeorm/entities/Product';
 
 import ICreateProductDTO from '../dtos/ICreateProductDTO';
+import IFindProductsDTO from '../dtos/IFindProductsDTO';
 import IUpdateProductsQuantityDTO from '../dtos/IUpdateProductsQuantityDTO';
-
-interface IFindProducts {
-  id: string;
-}
 
 export default interface IProductsRepository {
   create(data: ICreateProductDTO): Promise<Product>;
   findByName(name: string): Promise<Product | undefined>;
-  findAllById(products: IFindProducts[]): Promise<Product[]>;
+  findAllById(products: IFindProductsDTO[]): Promise<Product[]>;
   updateQuantity(products: IUpdateProductsQuantityDTO[]): Promise<Product[]>;
 }
